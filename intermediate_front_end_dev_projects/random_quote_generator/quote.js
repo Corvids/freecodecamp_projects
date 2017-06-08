e03f3cfac95b485ed1a67620d570f9df382d81c9
Corvids <joonnifer@gmail.com>
2017-06-08 16:50:38 -0500

    add bootstrap and attempt to fix js

--- intermediate_front_end_dev_projects/random_quote_generator/quote.js
+++ intermediate_front_end_dev_projects/random_quote_generator/quote.js
@@ -1,5 +1,28 @@
-$(document).ready(function() {
-  $("#getQuote").on("click", function(){
-    $(".quote").html("Insert a random quote here");
+$('#getQuote').on('click', function(e) {
+    e.preventDefault();
+    $.ajax( {
+      url: '/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+        $('#quote-title').text(post.title);
+        $('#quote-content').html(post.content);
+
+        // If the Source is available, use it. Otherwise hide it.
+        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+          $('#quote-source').html('Source:' + post.custom_meta.Source);
+        } else {
+          $('#quote-source').text('');
+        }
+      },
+      cache: false
+    });
   });
+});
+// quotes api used:
+// https://quotesondesign.com/api-v4-0/
+
+$(function () {
+    $.ajax({
+
+    });
 });
