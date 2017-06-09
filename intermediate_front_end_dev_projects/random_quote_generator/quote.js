5b6fbb39467839f06f3268fcc9a66bceb740e119
Corvids <joonnifer@gmail.com>
2017-06-09 11:00:04 -0500

    reformat json file and add getnewquote function"

--- intermediate_front_end_dev_projects/random_quote_generator/quote.js
+++ intermediate_front_end_dev_projects/random_quote_generator/quote.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
+  // generate a new quote on page load
+  getNewQuote();
+  // generate a new quote on button click
   $('#getQuote').on('click', function() {
-    $.getJSON("/json/quotes.json", function(json) {
-      var quotes = new Array("foo bar", "hello, world", "42");
-
-      var rand = quotes[Math.floor( Math.random() * quotes.length )];
-
-      $(".quote").html("Hello, World");
-
-    });
+    getNewQuote();
   });
 });
+
+// function to generate a new quote
+function getNewQuote() {
+  var returnQuote = '';
+  $.getJSON("/json/quotes.json", function(json) {
+    json = json.filter(function(val) {
+      var rand = Math.floor( Math.random() * val.length );
+      return (val[rand]);
+    });
+
+    json.format(function(val) {
+      returnQuote = val.quote + " -" + val.source;
+      return returnQuote;
+    });
+  });
+
+  $(".quote").html(returnQuote);
+
+};
