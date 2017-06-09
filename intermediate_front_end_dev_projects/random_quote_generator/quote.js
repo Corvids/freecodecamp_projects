eb6144109ac1b1ccb316880b0c0f1ea2938f6754
Corvids <joonnifer@gmail.com>
2017-06-09 09:55:41 -0500

    update random quote fn and re-id quote well

--- intermediate_front_end_dev_projects/random_quote_generator/quote.js
+++ intermediate_front_end_dev_projects/random_quote_generator/quote.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $('#getQuote').on('click', function() {
     $.getJSON("/json/quotes.json", function(json) {
+      var quotes = new Array("foo bar", "hello, world", "42");
+
+      var rand = quotes[Math.floor( Math.random() * quotes.length )];
+
+      $(".quote").html("Hello, World");
 
     });
   });
