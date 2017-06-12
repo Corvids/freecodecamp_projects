4641b51dd42a2acb46a7411272665590cc6dbdd8
Corvids <joonnifer@gmail.com>
2017-06-12 15:40:59 -0500

    .

--- intermediate_front_end_dev_projects/weather_machine/script.js
+++ intermediate_front_end_dev_projects/weather_machine/script.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
+  //declare variables for location
+  var latitude = null;
+  var longitude = null;
+
+  // get location data
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      //$("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
     });
   }
+
+  // temperature conversion
+  $('#tempToFahrenheit').click(function() {
+
+  });
+  $('#tempToCelsius').click(function() {
+
+  });
+
 });
