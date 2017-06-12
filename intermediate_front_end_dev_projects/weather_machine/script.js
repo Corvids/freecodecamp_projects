1bbecf9ff07a8cb985eef86870d04a398f5e3164
Corvids <joonnifer@gmail.com>
2017-06-12 16:44:21 -0500

    add getWeather

--- intermediate_front_end_dev_projects/weather_machine/script.js
+++ intermediate_front_end_dev_projects/weather_machine/script.js
@@ -2,14 +2,32 @@ $(document).ready(function() {
   //declare variables for location
   var latitude = null;
   var longitude = null;
+  var secretkey = 'XXXXXXX' //input secret key here
+  var language = 'English';
 
-  // get location data
+  // get location
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
     });
   }
+
+  // get data from API
+  // see https://darksky.net/dev/account for secret key
+  // dark sky doc: https://darksky.net/dev
+  var getWeather = function() {
+		return $.ajax({
+			url: 'https://api.darksky.net/forecast/',
+			data: {
+        'key' : secretkey,
+				'lat': latitude,
+				'lon': longitude,
+				'lang': language
+			},
+			dataType: 'json',
+		})
+	};
 
   // temperature conversion
   $('#tempToFahrenheit').click(function() {
@@ -19,4 +37,5 @@ $(document).ready(function() {
 
   });
 
+  // end document ready
 });
