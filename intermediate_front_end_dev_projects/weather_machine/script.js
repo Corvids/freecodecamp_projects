79dbdb3a70ee75bf05c26ff941efd3d025d28cd5
Corvids <joonnifer@gmail.com>
2017-06-13 16:53:26 -0500

    update url in getWeather

--- intermediate_front_end_dev_projects/weather_machine/script.js
+++ intermediate_front_end_dev_projects/weather_machine/script.js
@@ -4,13 +4,17 @@ $(document).ready(function() {
   var longitude = null;
   var secretkey = 'XXXXXXX'; //input secret key here
   var language = 'English';
+  var sevenDayWeather = [];
 
   // get location
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
+      $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
     });
+    // if valid location, get weather forecast
+    getWeather();
   }
 
   // get data from API
@@ -18,11 +22,11 @@ $(document).ready(function() {
   // dark sky doc: https://darksky.net/dev
   var getWeather = function() {
 		return $.ajax({
-			url: 'https://api.darksky.net/forecast/',
+			url: 'https://api.darksky.net/forecast/' + secretkey + '/' + latitude + ',' + longitude,
 			data: {
+        'latitude': latitude,
+        'longitude': longitude,
         'key' : secretkey,
-				'lat': latitude,
-				'lon': longitude,
 				'lang': language
 			},
 			dataType: 'json',
