cab796977cfeebb2bb95e3a42f0b7d3a37e140c9
Corvids <joonnifer@gmail.com>
2017-06-14 15:11:08 -0500

    update getWeather

--- intermediate_front_end_dev_projects/weather_machine/script.js
+++ intermediate_front_end_dev_projects/weather_machine/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   //declare variables for location
   var latitude = null;
   var longitude = null;
-  var secretkey = 'XXXXXXX'; //input secret key here
+  var secretkey = 'da02804ce924f559722bbce05f35661e'; //input secret key here
   var language = 'English';
   var sevenDayWeather = [];
 
@@ -19,19 +19,19 @@ $(document).ready(function() {
 
   // get data from API
   // see https://darksky.net/dev/account for secret key
-  // dark sky doc: https://darksky.net/dev
-  var getWeather = function() {
-		return $.ajax({
-			url: 'https://api.darksky.net/forecast/' + secretkey + '/' + latitude + ',' + longitude,
-			data: {
-        'latitude': latitude,
-        'longitude': longitude,
-        'key' : secretkey,
-				'lang': language
-			},
-			dataType: 'json',
-		})
-	};
+  // dark sky doc: https://darksky.net/dev/docs/forecast
+  var getWeather = $(function (){
+    var $data = $('#data');
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.darksky.net/forecast/' + secretkey + '/' + latitude + ',' + longitude
+      success: function(weatherData) {
+        $.each(weatherData, function(i, item){
+          $data.html('<li>temp</li>');
+        });
+      }
+    });
+  });
 
   // temperature conversion
   $('#tempToFahrenheit').click(function() {
