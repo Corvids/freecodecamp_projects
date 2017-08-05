2e9397d60f5fe6a04b571d5b4e67b952f944091b
Jennifer Joo (corvids) <joonnifer@gmail.com>
2017-08-05 00:15:46 -0400

    moved api call

--- intermediate_front_end_dev_projects/weather_machine/script.js
+++ intermediate_front_end_dev_projects/weather_machine/script.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
   var sevenDayWeather = [];
   var tempType = 'Fahrenheit';
   var secretkey = 'da02804ce924f559722bbce05f35661e'; //input secret key here
+  var api = 'https://api.darksky.net/forecast/da02804ce924f559722bbce05f35661e/37.7749,122.4194';
 
   // get location
   if (navigator.geolocation) {
@@ -13,23 +14,20 @@ $(document).ready(function() {
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
       $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-    })
-  } else {
-    console.log('Sorry, we couldn\'t find your location!  We\'ll set a default location instead...');
-    latitude = '40.7614';
-    longitude = '73.9776';
-  };
+      api = 'https://api.darksky.net/forecast/'+secretkey+'/'+latitude+','+longitude;
+      console.log('api calling with...', api);
 
-  var api = 'https://api.darksky.net/forecast/'+secretkey+'/'+latitude+','+longitude;
-  console.log('api calling with...', api);
+      // get data from API
+      // see https://darksky.net/dev/account for secret key
+      // dark sky doc: https://darksky.net/dev/docs/forecast
+      $.getJSON(api, function(data){
+        console.log('api call was successful!');
+        var currentWeather = data.currently;
+        console.log(currentWeather);
+      });
+    }) //endof geolocation
+  }
 
-  // get data from API
-  // see https://darksky.net/dev/account for secret key
-  // dark sky doc: https://darksky.net/dev/docs/forecast
-  $.getJSON(api, function(data){
-    console.log('api call was successful!');
-    var currentWeather = data.currently;
-  });
 
   // temperature conversion
   $('#tempToFahrenheit').click(function() {
