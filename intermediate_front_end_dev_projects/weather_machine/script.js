852c44fb7e1dc2bcf85127ccbb7a417803d7cdfb
Jennifer Joo (corvids) <joonnifer@gmail.com>
2017-08-04 23:44:38 -0400

    remove getWeather fn

--- intermediate_front_end_dev_projects/weather_machine/script.js
+++ intermediate_front_end_dev_projects/weather_machine/script.js
@@ -12,9 +12,7 @@ $(document).ready(function() {
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
       $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-    });
-    // if valid location, get weather forecast
-    getWeather();
+    })
   } else {
     console.log('Sorry, we couldn\'t find your location!  We\'ll set a default location instead...');
     latitude = '40.7614';
@@ -23,25 +21,13 @@ $(document).ready(function() {
 
   var secretkey = 'da02804ce924f559722bbce05f35661e'; //input secret key here
   var api = 'https://api.darksky.net/forecast/'+secretkey+'/'+latitude+','+longitude;
+  console.log(api);
 
   // get data from API
   // see https://darksky.net/dev/account for secret key
   // dark sky doc: https://darksky.net/dev/docs/forecast
   $.getJSON(api, function(data){
     console.log('api call was successful!');
-  });
-
-  var getWeather = $(function (){
-    var $data = $('#data');
-    $.ajax({
-      type: 'GET',
-      url: 'https://api.darksky.net/forecast/' + secretkey + '/' + latitude + ',' + longitude
-      success: function(weatherData) {
-        $.each(weatherData, function(i, item){
-          $data.html('<li>temp</li>');
-        });
-      }
-    });
   });
 
   // temperature conversion
