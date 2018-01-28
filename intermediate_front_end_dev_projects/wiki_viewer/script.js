3beacbe29acebeb426b64f2955bb2692ee68b04f
corvids <joonnifer@gmail.com>
2018-01-27 23:11:33 -0600

    update script

--- intermediate_front_end_dev_projects/wiki_viewer/script.js
+++ intermediate_front_end_dev_projects/wiki_viewer/script.js
@@ -1,52 +1,43 @@
 $(document).ready(function() {
-  var app = angular.module('WikiApp', ['ngAnimate']);
-  app.controller('MainCtrl', function($scope, $http, $timeout) {
-    var form = $('form');
-    var close = $('.eks');
-    var input = $('input');
-    var search = $("#search");
-    var help = $("#help");
 
-    $scope.results = [];
+  var searchResults = [];
+  var html = '';
 
-    close.on('click', function() {
-      form.toggleClass('open');
+  function Result(title, snippet) {
+    this.title = title;
+    this.snippet = snippet;
+  }
 
-      if (!form.hasClass('open') && $scope.searchTxt !== '' && typeof $scope.searchTxt !== 'undefined') {
-  	    search.toggleClass('fullHeight')
-        help.toggleClass('hide');
-        $scope.searchTxt = '';
+  function search() {
+    $.ajax({
+      url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + $('#search').val(),
+      dataType: 'jsonp',
+      type: 'POST',
+      headers: {
+        'Api-User-Agent': 'Example/1.0'
+      },
+      success: function(data) {
+
+        $('.results').empty();
+
+        var resArr = data.query.search;
+
+        for (var result in resArr) {
+          searchResults.push(new Result(resArr[result].title, resArr[result].snippet));
+          html = '<div id="articles" class="well"><a href="https://en.wikipedia.org/wiki/' + resArr[result].title + '"target="_blank"><h3>' + resArr[result].title + '</h3><p>' + resArr[result].snippet + '</p></a></div>';
+
+          $('.results').append(html);
+        }
       }
-      $scope.results = [];
-      $scope.$apply();
-    })
+    });
 
-    input.on('transitionend webkitTransitionEnd oTransitionEnd', function() {
-      if (form.hasClass('open')) {
-        input.focus();
-      } else {
-        return;
-      }
-    })
+  }
 
-    $scope.search = function() {
-      $scope.results = [];
-      help.addClass('hide');
-      search.removeClass('fullHeight');
-      var title = input.val();
-      var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
-      var cb = '&callback=JSON_CALLBACK';
-      var page = 'https://en.wikipedia.org/?curid=';
-
-      $http.jsonp(api + title + cb)
-      .success(function(data) {
-        var results = data.query.pages;
-        angular.forEach(results, function(v,k)  {
-          $scope.results.push({title: v.title, body: v.extract, page: page + v.pageid})
-        })
-      });
-    }
+  $('#search').click(function() {
+    search();
+  });
+  $('#search').keyup(function() {
+    search();
   });
 
-  // end document ready
 });
