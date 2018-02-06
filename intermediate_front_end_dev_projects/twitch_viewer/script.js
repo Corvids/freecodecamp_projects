3a6975e20961aa2a9bacfc4f10e567585e84664f
corvids <joonnifer@gmail.com>
2018-02-06 15:48:13 -0600

    refactor js file

--- intermediate_front_end_dev_projects/twitch_viewer/script.js
+++ intermediate_front_end_dev_projects/twitch_viewer/script.js
@@ -5,10 +5,10 @@ function getChannelInfo() {
     function makeURL(type, name) {
       return 'https://wind-bow.gomix.me/twitch-api/' + type + '/' + name + '?callback=?';
     };
-    
+
     $.getJSON(makeURL("streams", channel), function(data) {
       let game, status;
-      
+
       if (data.stream === null || data.stream === undefined) {
         game = "offline";
         status = "offline";
@@ -16,19 +16,44 @@ function getChannelInfo() {
         game = data.stream.game;
         status = "online";
       };
-      
+
       $.getJSON(makeURL("channels", channel), function(data) {
-        var logo = data.logo != null ? data.logo : "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F",
-          name = data.display_name != null ? data.display_name : channel,
-          description = status === "online" ? ': ' + data.status : "";
-          html = '<div class="row ' + 
-          status + '"><div class="col-xs-2 col-sm-1" id="icon"><img src="' + 
-          logo + '" class="logo"></div><div class="col-xs-10 col-sm-3" id="name"><a href="' + 
-          data.url + '" target="_blank">' + 
-          name + '</a></div><div class="col-xs-10 col-sm-8" id="streaming">'+ 
-          game + '<span class="hidden-xs">' + 
-          description + '</span></div></div>';
-        status === "online" ? $("#display").prepend(html) : $("#display").append(html);
+        let logo, name, description, html;
+
+        if( data.logo != null ) {
+          logo = data.logo;
+        } else {
+          logo = 'http://via.placeholder.com/50x50';
+        }
+
+        if( data.display_name != null ) {
+          name = data.display_name
+        } else {
+          name = channel;
+        }
+
+        if( status === 'online' ){
+          html = '<div class="row ' +
+            status + '"><div class="col-xs-2 col-sm-1" id="icon"><img src="' +
+            logo + '" class="logo"></div><div class="col-xs-10 col-sm-3" id="name"><a href="' +
+            data.url + '" target="_blank">' +
+            name + '</a></div><div class="col-xs-10 col-sm-8" id="streaming">'+
+            game + '<span class="hidden-xs">' +
+            ': ' + data.status + '</span></div></div>';
+
+          $("#display").prepend(html)
+        } else {
+          html = '<div class="row ' +
+            status + '"><div class="col-xs-2 col-sm-1" id="icon"><img src="' +
+            logo + '" class="logo"></div><div class="col-xs-10 col-sm-3" id="name"><a href="' +
+            data.url + '" target="_blank">' +
+            name + '</a></div><div class="col-xs-10 col-sm-8" id="streaming">'+
+            game + '<span class="hidden-xs">' +
+            '</span></div></div>';
+
+          $("#display").append(html);
+        }
+
       });
     });
   });
