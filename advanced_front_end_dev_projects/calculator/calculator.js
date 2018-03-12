f3c09139d273091335909d36a1cb30fc9da1925e
corvids <joonnifer@gmail.com>
2018-03-11 21:15:15 -0500

    wip

--- advanced_front_end_dev_projects/calculator/calculator.js
+++ advanced_front_end_dev_projects/calculator/calculator.js
@@ -5,13 +5,17 @@ $(document).ready(function() {
   $('button').click(function() {
     let entry = $(this).attr("value");
     console.log('you clicked on ' + entry);
+    
+    // clear all calculations
     if( entry === 'All Clear' ) {
       ans = null;
       console.log('cleared all')
+    // clear the last calculation only
     } else if( entry === 'Clear Entry' ) {
       current = null;
     } else {
-      
+      // pressed a calculation key
+      if( entry==='/' || entry==='*' || )
     }
     
   });
