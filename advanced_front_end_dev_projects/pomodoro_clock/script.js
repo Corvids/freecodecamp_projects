cc291125a95af502955d507c286dce2d7da8c79d
corvids <joonnifer@gmail.com>
2018-04-10 20:39:01 -0700

    add functions to change timers for break and session

--- advanced_front_end_dev_projects/pomodoro_clock/script.js
+++ advanced_front_end_dev_projects/pomodoro_clock/script.js
@@ -1,4 +1,20 @@
 $('document').ready(function(){
+  let break_timer = 5;
+  let session_timer = 25;
+  
+  // change session and break times
+  $( "#break-minus" ).click(function() {
+    break_timer -=1;
+  });
+  $( "#break-plus" ).click(function() {
+    break_timer +=1;
+  });
+  $( "#session-minus" ).click(function() {
+    session_timer -=1;
+  });
+  $( "#session-plus" ).click(function() {
+    session_timer +=1;
+  });
   
   
 });
