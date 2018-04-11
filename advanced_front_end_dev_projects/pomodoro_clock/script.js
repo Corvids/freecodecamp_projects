2869f4742dd6b9eb99ab01b65182b197fdb766f2
corvids <joonnifer@gmail.com>
2018-04-10 20:46:50 -0700

    fix buttons id

--- advanced_front_end_dev_projects/pomodoro_clock/script.js
+++ advanced_front_end_dev_projects/pomodoro_clock/script.js
@@ -2,21 +2,26 @@ $('document').ready(function(){
   let break_timer = 5;
   let session_timer = 25;
   
+  // show on loading page
+  $('.break-timer').append(break_timer);
+  $('.session-timer').append(session_timer);
+  
   // change session and break times
   $( "#break-minus" ).click(function() {
     break_timer -=1;
+    $('.break-timer').html(break_timer);
   });
   $( "#break-plus" ).click(function() {
     break_timer +=1;
+    $('.break-timer').html(break_timer);
   });
   $( "#session-minus" ).click(function() {
     session_timer -=1;
+    $('.session-timer').html(session_timer);
   });
   $( "#session-plus" ).click(function() {
     session_timer +=1;
+    $('.session-timer').html(session_timer);
   });
-  
-  $('.break-timer').append(break_timer);
-  $('.session-timer').append(session_timer);
   
 });
