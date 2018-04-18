1cfa3f1c2bf511376d5097fe4d691fbc6c76d954
corvids <joonnifer@gmail.com>
2018-04-17 21:35:22 -0700

    finish function to format time left

--- advanced_front_end_dev_projects/pomodoro_clock/script.js
+++ advanced_front_end_dev_projects/pomodoro_clock/script.js
@@ -2,11 +2,11 @@ $('document').ready(function(){
   let break_timer = 5;
   let session_timer = 25;
   let time_left = 25;
-  
+
   // show on loading page
   $('.break-timer').append(break_timer);
   $('.session-timer').append(session_timer);
-  
+
   // change session and break times
   $( "#break-minus" ).click(function() {
     if( break_timer > 0) break_timer -=1;
@@ -28,27 +28,34 @@ $('document').ready(function(){
     time_left = session_timer;
     $('.session-timer').html(session_timer);
   });
-  
+
   // clicking timer button will start the count-down
   $( "#timer" ).click(function() {
     $("timer").html(session_timer);
   });
-  
+
   function formatTimer(time) {
     let t = Number(time);
     let time_string = '';
-    
+
     let hours = Math.floor(t / 3600);
-    if( hours > 0 ) time_string += hours.toString() + ':'
-    
+    if( hours > 0 ) time_string += hours.toString() + ' : ';
+
     let minutes = Math.floor(t % 3600 / 60);
-    if( minutes < 10 ) P{}
-    
+    if( minutes < 10 ) {
+      time_string += "0" + minutes.toString() + ' : ';
+    } else {
+      time_string += minutes.toString + ' : ';
+    }
+
     let seconds = Math.floor(t % 3600 % 60);
-    
-    return (
-      (h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s
-    ); 
+    if( seconds < 10 ) {
+      time_string += "0" + seconds.toString();
+    } else {
+      time_string += seconds.toString;
+    }
+
+    return time_string;
   }
-  
+
 });
